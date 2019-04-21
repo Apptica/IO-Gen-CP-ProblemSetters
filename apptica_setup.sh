aabb9c65e1ee1b4a25e46f70d3ecade0d1b8d5e6
Saurabh Joshi <saurabh.joshi.cse@gmail.com>
2019-04-21 11:21:58 +0530

    Add files via upload

---
+++ apptica_setup.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt-get install 
+sudo apt-get install code
+sudo apt-get install i3
+sudo apt-get install i3-lock
+sudo apt-get install i3blocks
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt update
+sudo apt install oracle-java8-installer
+mkdir ~/codechef ~/codeforces ~/spoj ~/hackerearth ~/topcoder ~/google ~/books ~/leetcode
+sudo apt install wget
+wget https://github.com/dj3500/hightail/releases/download/v0.9.7/Hightail-v0.9.7.jar ~/codeforces
+wget http://www.topcoder.com/contest/arena/ContestAppletProd.jnlp ~/topcoder
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb ~/Downloads
+sudo dpkg -i ~/Downloads/g*.deb
+rm ~/Downloads/g*.deb
+sudo apt install -f
